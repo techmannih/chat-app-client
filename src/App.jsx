3ed213e2e9d24b28563e23ea683c015c31185385
Ankit Ydv <ankityadav11241@gmail.com>
2023-11-21 11:26:26 +0530

    setup tailwind-css

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,7 @@
-import { useState } from 'react'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-    <div>Hello Ankit!!</div>
-    </>
+    <h1 className="text-3xl text-yellow-500 font-bold">
+      Hello Ankit!
+    </h1>
   )
 }
-
-export default App

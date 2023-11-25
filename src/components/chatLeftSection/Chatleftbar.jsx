f56fb4b76b77ee82479b6073146280be9a0dc8a1
Ankit Ydv <ankityadav11241@gmail.com>
2023-11-25 13:16:29 +0530

    user chats

--- src/components/chatLeftSection/Chatleftbar.jsx
+++ src/components/chatLeftSection/Chatleftbar.jsx
@@ -1,14 +1,36 @@
 import ChatSearch from "../chatSearch/ChatSearch";
+import ChatUser from "../chatUser/ChatUser";
 import CurrentUser from "../currentUser/CurrentUser";
 
 export default function Chatleftbar() {
   return (
-    <div className="w-1/3 h-full bg-slate-100">
-      <div className="top">
+    <div className="w-1/3 h-full bg-slate-100 ">
+      <div className="z-10">
         <CurrentUser />
       </div>
-      <div className="leftBottom">
+      <div>
         <ChatSearch />
+      </div>
+      <div className="leftUsers mt-5">
+        <ChatUser/>
+        <ChatUser/>
+        <ChatUser/>
+        <ChatUser/>
+        <ChatUser/>
+        <ChatUser/>
+        <ChatUser/>
+        <ChatUser/>
+        <ChatUser/>
+        <ChatUser/>
+        <ChatUser/>
+        <ChatUser/>
+        <ChatUser/>
+        <ChatUser/>
+        <ChatUser/>
+        <ChatUser/>
+        <ChatUser/>
+        <ChatUser/>
+        <ChatUser/>
       </div>
     </div>
   );
---
+++ src/components/chatUser/ChatUser.jsx
@@ -0,0 +1,35 @@
+export default function ChatUser() {
+  return (
+    <div className="container flex p-4">
+      {/* profile pic */}
+      <div className="w-20 h-20">
+        <img
+          className="w-16 h-16 rounded-full"
+          src="/src/assets/img/profile.jpg"
+          alt=""
+        />
+      </div>
+      <div className="container ml-4">
+        {/* name and time */}
+        <div className="flex overflow-hidden content-center justify-between">
+          <div className="text-blue-800 text-lg font-bold">User Fullname</div>
+          <div>10:32 AM</div>
+        </div>
+        {/* messages and  notification */}
+        <div className="flex">
+          <div className="container">
+            <p className="line-clamp-2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem,
+              perferendis! Lorem ipsum, dolor sit amet consectetur adipisicing
+              elit. Ex, cumque facilis minus vitae illum corporis sit esse iure
+              ullam eos!
+            </p>
+          </div>
+          <div className="bg-blue-500 rounded-full w-7 h-7 relative">
+            <span className="absolute left-2 top-1">3</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}

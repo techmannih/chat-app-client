7a21e1f4eba4b00ec41ac03fad9b6bae6ad095b5
Ankit Ydv <ankityadav11241@gmail.com>
2023-11-27 13:49:06 +0530

    fix: chats scroll

--- src/components/chatLeftSection/Chatleftbar.jsx
+++ src/components/chatLeftSection/Chatleftbar.jsx
@@ -4,14 +4,14 @@ import CurrentUser from "../currentUser/CurrentUser";
 
 export default function Chatleftbar() {
   return (
-    <div className="w-1/3 h-full bg-slate-100 ">
+    <div className="w-1/3 h-full bg-slate-100 overflow-hidden">
       <div className="z-10">
         <CurrentUser />
       </div>
       <div>
         <ChatSearch />
       </div>
-      <div className="leftUsers mt-5">
+      <div className="leftUsers mt-5 overflow-scroll h-full">
         <ChatUser/>
         <ChatUser/>
         <ChatUser/>

de88f843db6bdabeaa30918a071bafd2c1d3dc6f
Ankit Ydv <ankityadav11241@gmail.com>
2023-12-04 11:53:57 +0530

    env set

--- src/fireauth/firebase_auth.js
+++ src/fireauth/firebase_auth.js
@@ -7,9 +7,9 @@ import { GoogleAuthProvider, getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
-  authDomain: "chat-app-dec.firebaseapp.com",
-  projectId: "chat-app-dec",
-  storageBucket: "chat-app-dec.appspot.com",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGEID,
   appId: import.meta.env.VITE_FIREBASE_APPID
 };

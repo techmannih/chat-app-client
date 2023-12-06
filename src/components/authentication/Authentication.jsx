7dc5398fd4220865a8e7dc677f001673b4b76b37
Ankit Ydv <ankityadav11241@gmail.com>
2023-12-06 12:04:37 +0530

    add: redirect to main

--- src/components/authentication/Authentication.jsx
+++ src/components/authentication/Authentication.jsx
@@ -2,33 +2,37 @@ import React, { useState } from 'react';
 import { googleSignIn } from '../../fireauth/firebase_auth';
 import { signInWithPopup, signInWithRedirect } from 'firebase/auth';
 import { auth } from '../../fireauth/firebase_auth';
-import { createUser } from '../../utils/APIs';
+import { createUser, setCookies } from '../../utils/APIs';
+import { useNavigate } from 'react-router-dom';
 
-const Signup = () => {
-    const [error, setError] = useState('Error Signing In !!');
+const Authenticate = () => {
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
+
     const handleSignup = async () => {
-        try {
-            await signInWithPopup(auth, googleSignIn)
-                .then((resp) => {
-                    console.log(resp);
-                    const reqData = {
-                        fullname: resp.user.displayName,
-                        userId: resp.user.uid,
-                        profilePicUrl: resp.user.photoURL,
-                        email: resp.user.email
-                    }
-                    const res = createUser(reqData);
-                    if(res === true){
-                        console.log('User created successfully');
-                    }else{
-                        console.log('User not created');
-                    }
-                }).catch((error) => {
-                    console.log(error);
-                })
-        } catch (error) {
-            console.error('Error signing up:', error);
-        }
+        await signInWithPopup(auth, googleSignIn)
+            .then((resp) => {
+                console.log(resp);
+                const reqData = {
+                    fullname: resp.user.displayName,
+                    userId: resp.user.uid,
+                    profilePicUrl: resp.user.photoURL,
+                    email: resp.user.email
+                }
+                const res = createUser(reqData);
+                if (res === true) {
+                    console.log('User created successfully');
+                    setCookies('userId', resp.user.uid, 1);
+                    setError('');
+                    navigate('/', { replace: true });
+                } else {
+                    console.log('User not created');
+                    setError('Error occured !!');
+                }
+            }).catch((error) => {
+                console.log(error);
+                setError('Error occured !!');
+            });
     };
 
     return (
@@ -38,9 +42,7 @@ const Signup = () => {
                 <div className="w-36 h-36 m-auto">
                     <img src="src/assets/img/chat.png" alt="" />
                 </div>
-                {
-                    error && <div className="text-red-200 text-center text-2xl mt-4">{error}</div>
-                }
+                <div className="text-red-200 text-center text-2xl mt-4">{error}</div>
                 <div className="bg-gradient-to-r from-blue-400 via-purple-300 to-blue-500 flex items-center justify-center p-3 w-2/3 rounded-xl mt-14 cursor-pointer hover:bg-gradient-to-r hover:from-indigo-400 hover:via-purple-400 hover:to-blue-500" onClick={handleSignup}>
                     <div className="w-12 h-12 mx-8">
                         <img src="src/assets/img/google.png" alt="" />
@@ -52,4 +54,4 @@ const Signup = () => {
     );
 };
 
-export default Signup;
+export default Authenticate;

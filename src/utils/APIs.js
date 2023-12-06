ad47db8fd08ab40830d21bcef8b79f9ba1ac95b0
Ankit Ydv <ankityadav11241@gmail.com>
2023-12-06 12:04:10 +0530

    add: cookies methods

--- src/utils/APIs.js
+++ src/utils/APIs.js
@@ -3,10 +3,28 @@ import axios from "axios";
 // function to hit createUser API endpoint
 export const createUser = async (data) => {
   const user = await axios.post(`${import.meta.env.VITE_SERVER_URI}/users/new`, data);
-  if(user){
+  if (user) {
     return true;
-  }else{
+  } else {
     return false;
   }
 }
 
+export const setCookies = (key, value) => {
+  document.cookie = `${key}=${value}`;
+}
+
+export const removeCookies = (key) => {
+  document.cookie = `${key}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+}
+
+export const getCookies = (key) => {
+  const cookies = document.cookie.split(';');
+  for (let i = 0; i < cookies.length; i++) {
+    const cookie = cookies[i].split('=');
+    if (cookie[0] === key) {
+      return cookie[1];
+    }
+  }
+  return null;
+}

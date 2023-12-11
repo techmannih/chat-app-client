45ee77db4954fc88416f20cc2079a2671157f1c3
Ankit Ydv <ankityadav11241@gmail.com>
2023-12-11 15:12:40 +0530

    add: chat blank page

---
+++ src/components/chatBlank/ChatBlank.jsx
@@ -0,0 +1,14 @@
+export default function ChatBlank() {
+    return (
+        <>
+            <div className="w-2/3 h-full">
+                <div className="flex items-center justify-center h-full">
+                    <div className="">
+                        <img className="w-80 h-80" src="src/assets/img/start.gif" alt="" />
+                        <div className="text-xl font-bold text-blue-500">Select a chat to start messaging</div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}

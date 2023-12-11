a2886aea6b15033d2f7368a00a8e1304baba7a5d
Ankit Ydv <ankityadav11241@gmail.com>
2023-12-11 15:13:17 +0530

    fix: routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
+import { useState } from "react";
 import Chatmain from "./components/chatmain/Chatmain";
-import CurrentUser from "./components/currentUser/CurrentUser";
+import { useEffect } from "react";
+import { getCookies } from "./utils/APIs";
+import Authenticate from "./components/authentication/Authentication";
 
 export default function App() {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const userIdValue = getCookies("userId");
+    if (userIdValue !== 'null') {
+      setIsLoggedIn(true);
+    } else {
+      setIsLoggedIn(false);
+    }
+  }, []);
+
   return (
     <>
-      <div
-        className="
-          font-montserrat 
-          h-screen
-          overflow-hidden"
-      >
-        <Chatmain />
+      <div className="font-montserrat h-screen overflow-hidden">
+        {!isLoggedIn && <Authenticate isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />}
+        {isLoggedIn && <Chatmain />}
       </div>
     </>
   );

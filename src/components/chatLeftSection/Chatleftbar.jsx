798ea6b3bef61c2809f9592e61d2aadafdd5163f
Ankit Ydv <ankityadav11241@gmail.com>
2023-12-13 16:14:47 +0530

    add: props

--- src/components/chatLeftSection/Chatleftbar.jsx
+++ src/components/chatLeftSection/Chatleftbar.jsx
@@ -2,11 +2,11 @@ import ChatSearch from "../chatSearch/ChatSearch";
 import ChatUser from "../chatUser/ChatUser";
 import CurrentUser from "../currentUser/CurrentUser";
 
-export default function Chatleftbar() {
+export default function Chatleftbar({ userData, chatsList }) {
   return (
     <div className="w-1/3 h-full bg-slate-100 overflow-hidden">
       <div className="z-10">
-        <CurrentUser />
+        <CurrentUser userData={userData} />
       </div>
       <div>
         <ChatSearch />
@@ -14,26 +14,17 @@ export default function Chatleftbar() {
       <div className="mt-4">
         <hr className="border-slate-200" />
       </div>
-      <div className="leftUsers overflow-scroll h-full">
-        <ChatUser />
-        <ChatUser />
-        <ChatUser />
-        <ChatUser />
-        <ChatUser />
-        <ChatUser />
-        <ChatUser />
-        <ChatUser />
-        <ChatUser />
-        <ChatUser />
-        <ChatUser />
-        <ChatUser />
-        <ChatUser />
-        <ChatUser />
-        <ChatUser />
-        <ChatUser />
-        <ChatUser />
-        <ChatUser />
-        <ChatUser />
+      <div className="overflow-scroll h-full">
+        {
+          chatsList.chats.length === 0 ? <div className="flex justify-center p-1 mt-14 text-xl">Add users...</div>
+            : chatsList.chats.map((chat) => {
+              return (
+                <div className="flex justify-center text-xl" key={chat._id}>
+                  <ChatUser chat={chat} />
+                </div>
+              );
+            })
+        }
       </div>
     </div>
   );

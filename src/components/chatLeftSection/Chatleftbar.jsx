14dbf0d8c175a0a42407e9864c6c943767e9053c
Ankit Ydv <ankityadav11241@gmail.com>
2023-12-15 16:21:34 +0530

    add: onclick

--- src/components/chatLeftSection/Chatleftbar.jsx
+++ src/components/chatLeftSection/Chatleftbar.jsx
@@ -3,6 +3,11 @@ import ChatUser from "../chatUser/ChatUser";
 import CurrentUser from "../currentUser/CurrentUser";
 
 export default function Chatleftbar({ userData, chatsList }) {
+
+  const handleGetChatArea = (chatId) => {
+    window.location.href = `${chatId}`;
+  }
+
   return (
     <div className="w-1/3 h-full bg-slate-100 overflow-hidden">
       <div className="z-10">
@@ -19,7 +24,7 @@ export default function Chatleftbar({ userData, chatsList }) {
           chatsList.chats.length === 0 ? <div className="flex justify-center p-1 mt-14 text-xl">Add users...</div>
             : chatsList.chats.map((chat) => {
               return (
-                <div className="flex justify-center text-xl" key={chat._id}>
+                <div className="flex justify-center text-xl" key={chat._id} onClick={()=>handleGetChatArea(chat._id)}>
                   <ChatUser chat={chat} />
                 </div>
               );

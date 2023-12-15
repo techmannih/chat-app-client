854fd954359d7ae01c40eeb6bce1d56c5d22144e
Ankit Ydv <ankityadav11241@gmail.com>
2023-12-15 16:18:34 +0530

    add: profile elements

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,13 +1,43 @@
-export default function Profile() {
+import { useEffect, useState } from "react";
+import { getCookies, removeCookies } from "../../utils/APIs";
+import axios from "axios";
+
+export function Profile({userData}) {
+
+    const handleLogout = () => {
+        removeCookies("userId");
+        window.location.href = "/auth";
+    }
+
+    const deleteAccount = async () => {
+        // fetch user data from backend using axios
+        const response = await axios.delete(`${import.meta.env.SERVER_URI}/users/delete/${getCookies("userId")}`);
+        console.log(response);
+    }
+
+    useEffect(() => {
+        if (getCookies("userId") === 'null') {
+            window.location.href = "/";
+        }
+    }, [])
+
+    if (userData === null) {
+        return (
+            <div className="">Loading...</div>
+        )
+    }
+
     return (
         <div className="w-full h-full">
             <div className="mt-20">
                 <div className="flex justify-center">
-                    <div className="w-60 h-60 rounded-full bg-gray-300"></div>
+                    <div className="w-60 h-60 rounded-full bg-gray-300">
+                        <img className="w-full rounded-full" src={userData.profilePicUrl} alt="profile picture" />
+                    </div>
                 </div>
                 <div className="flex justify-center mt-6">
                     <div className="text-center">
-                        <h1 className="text-4xl text-black font-extrabold">Name</h1>
+                        <h1 className="text-4xl text-black font-extrabold">{userData.fullname}</h1>
                     </div>
                 </div>
             </div>
@@ -18,12 +48,12 @@ export default function Profile() {
                 <div className="flex justify-center w-full text-violet-800">
                     <div className="w-1/2 text-xl">
                         <div className="flex justify-between">
-                            <div className="font-bold">Email</div>
-                            <div className="font-bold">mailId</div>
+                            <div className="font-bold">Username</div>
+                            <div className="font-bold">{userData.username}</div>
                         </div>
-                        <div className="flex justify-between mt-4">
-                            <div className="font-bold">Address</div>
-                            <div className="font-bold">address</div>
+                        <div className="flex justify-between">
+                            <div className="font-bold">Email</div>
+                            <div className="font-bold">{userData.email}</div>
                         </div>
                     </div>
                 </div>
@@ -31,11 +61,11 @@ export default function Profile() {
             <div className="flex justify-center mt-12">
                 <div className="w-1/2">
                     <div className="flex justify-between">
-                        <div className="text-xl font-extrabold bg-red-600 text-white hover:bg-red-500 p-2 rounded-md cursor-pointer">Delete Account</div>
-                        <div className="text-xl font-extrabold bg-blue-700 p-2 text-white rounded-md cursor-pointer hover:bg-blue-500">Logout</div>
+                        <div className="text-xl font-extrabold bg-red-600 text-white hover:bg-red-500 p-2 rounded-md cursor-pointer" onClick={deleteAccount}>Delete Account</div>
+                        <div className="text-xl font-extrabold bg-blue-700 p-2 text-white rounded-md cursor-pointer hover:bg-blue-500" onClick={handleLogout}>Logout</div>
                     </div>
                 </div>
-            </div>  
+            </div>
         </div>
     )
 }
